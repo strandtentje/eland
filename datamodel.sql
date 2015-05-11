1b1a4b737f4413ec087d41bb2cbe0bfd0d310872
Rob Tierolff <teh@borreh.nl>
2015-05-11 22:50:54 +0200

    Datamodel

---
+++ datamodel.sql
@@ -0,0 +1,96 @@
+-- MySQL dump 10.15  Distrib 10.0.11-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: stortneer
+-- ------------------------------------------------------
+-- Server version	10.0.11-MariaDB-1~wheezy
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Behaald`
+--
+
+DROP TABLE IF EXISTS `Behaald`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Behaald` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `team` int(11) NOT NULL,
+  `prestatie` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Behaald_1` (`team`),
+  KEY `fk_Behaald_2` (`prestatie`),
+  CONSTRAINT `fk_Behaald_2` FOREIGN KEY (`prestatie`) REFERENCES `Prestatie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Behaald_1` FOREIGN KEY (`team`) REFERENCES `Team` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Persoon`
+--
+
+DROP TABLE IF EXISTS `Persoon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Persoon` (
+  `int` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `voornaam` varchar(256) NOT NULL,
+  `tussenvoegsel` varchar(45) DEFAULT NULL,
+  `achternaam` varchar(256) NOT NULL,
+  `buddy` int(11) DEFAULT NULL,
+  `secret` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`int`),
+  KEY `persoonbuddy_idx` (`buddy`),
+  CONSTRAINT `persoonbuddy` FOREIGN KEY (`buddy`) REFERENCES `Persoon` (`int`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Prestatie`
+--
+
+DROP TABLE IF EXISTS `Prestatie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Prestatie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naam` varchar(64) NOT NULL,
+  `max` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Team`
+--
+
+DROP TABLE IF EXISTS `Team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Team` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naam` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-11 22:50:00
