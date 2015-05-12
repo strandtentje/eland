d0315641f00f86e70236e68410d8ffccd34e7c89
Rob Tierolff <teh@borreh.nl>
2015-05-13 01:40:15 +0200

    Added ranking and reduced amount of queries

--- score/queries/GetTeams.sql
+++ score/queries/GetTeams.sql
@@ -1,5 +1,16 @@
-SELECT
-	id as team, 
-	naam
+SET @rank=0;
+SELECT 
+	@rank:=@rank+1 AS rank, 
+	teams.*
 FROM
-	Team;
+	(SELECT
+	    Behaald.team team,
+	    Team.naam naam,
+		SUM(Behaald.waarde) totaal
+	FROM
+		Behaald JOIN Team ON
+	    Behaald.team = Team.id
+	GROUP BY
+	    Behaald.team
+	ORDER BY
+	    SUM(Behaald.waarde) DESC) teams;
