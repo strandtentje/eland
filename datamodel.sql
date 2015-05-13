bc12d2757ef1726bc64619a2593b27386640aafc
Rob Tierolff <teh@borreh.nl>
2015-05-13 02:34:18 +0200

    Aangepast datamodel

--- datamodel.sql
+++ datamodel.sql
@@ -26,12 +26,15 @@ CREATE TABLE `Behaald` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `team` int(11) NOT NULL,
   `prestatie` int(11) NOT NULL,
+  `waarde` int(11) DEFAULT NULL,
+  `moment` datetime DEFAULT NULL,
+  `obsolete` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_Behaald_1` (`team`),
   KEY `fk_Behaald_2` (`prestatie`),
-  CONSTRAINT `fk_Behaald_2` FOREIGN KEY (`prestatie`) REFERENCES `Prestatie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Behaald_1` FOREIGN KEY (`team`) REFERENCES `Team` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  CONSTRAINT `fk_Behaald_1` FOREIGN KEY (`team`) REFERENCES `Team` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Behaald_2` FOREIGN KEY (`prestatie`) REFERENCES `Prestatie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,7 +55,7 @@ CREATE TABLE `Persoon` (
   PRIMARY KEY (`int`),
   KEY `persoonbuddy_idx` (`buddy`),
   CONSTRAINT `persoonbuddy` FOREIGN KEY (`buddy`) REFERENCES `Persoon` (`int`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,8 +69,10 @@ CREATE TABLE `Prestatie` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `naam` varchar(64) NOT NULL,
   `max` int(11) DEFAULT NULL,
+  `slug` varchar(45) DEFAULT NULL,
+  `obsolete` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -80,8 +85,9 @@ DROP TABLE IF EXISTS `Team`;
 CREATE TABLE `Team` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `naam` varchar(64) NOT NULL,
+  `obsolete` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -93,4 +99,4 @@ CREATE TABLE `Team` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-11 22:50:00
+-- Dump completed on 2015-05-13  2:33:50
