3e73e87ed1a902851945a9017686193500e24077
Rob Tierolff <teh@borreh.nl>
2016-01-31 00:16:05 +0100

    Show only the most recent cells for a person

--- person/queries/GetPersonForView.sql
+++ person/queries/GetPersonForView.sql
@@ -4,13 +4,21 @@ SELECT
 FROM
 	PersonView
 	LEFT JOIN 
-		(SELECT 
-			PersonAttribute.name, 
-            PersonAttribute.value
-		FROM 
-			PersonAttribute 
-		WHERE 
-			person = @person) AS PersonAttribute
+		(SELECT
+			PersonAttribute.name,
+			PersonAttribute.value
+		FROM
+			(SELECT 			
+				MAX(id) id
+			FROM 
+				PersonAttribute 
+			WHERE 
+				person = @person
+			GROUP BY
+				PersonAttribute.name) AS LatestAttribute
+			LEFT JOIN 
+				PersonAttribute
+				ON LatestAttribute.id = PersonAttribute.id) AS PersonAttribute
 		ON PersonView.name = PersonAttribute.name
         
 WHERE
