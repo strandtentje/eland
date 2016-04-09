fe0a3ab838dbc3580a87035af5e620d6a3ceabe0
Rob Tierolff <teh@borreh.nl>
2016-04-09 22:15:15 +0200

    Moved db init sql's to subrepos

--- database/base.sql
+++ database/base.sql
@@ -105,27 +105,6 @@ SET character_set_client = utf8;
 SET character_set_client = @saved_cs_client;
 
 --
--- Table structure for table `GroupGrant`
---
-
-DROP TABLE IF EXISTS `GroupGrant`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `GroupGrant` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `groupname` varchar(45) DEFAULT NULL,
-  `resource` varchar(45) DEFAULT NULL,
-  `subresource` varchar(45) DEFAULT NULL,
-  `action` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `resix` (`resource`),
-  KEY `subresix` (`subresource`),
-  KEY `actionix` (`action`),
-  KEY `gnix` (`groupname`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `Locale`
 --
 
@@ -139,24 +118,6 @@ CREATE TABLE `Locale` (
   PRIMARY KEY (`id`),
   KEY `langct` (`language`,`country`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Login`
---
-
-DROP TABLE IF EXISTS `Login`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Login` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `passwordhash` varchar(512) DEFAULT NULL,
-  `person` int(11) DEFAULT NULL,
-  `password` varchar(250) DEFAULT NULL,
-  `salt` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -280,25 +241,6 @@ CREATE TABLE `TokenSection` (
   `section` varchar(45) NOT NULL,
   PRIMARY KEY (`section`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `UserGroup`
---
-
-DROP TABLE IF EXISTS `UserGroup`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `UserGroup` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `groupname` varchar(45) DEFAULT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `starts` datetime DEFAULT NULL,
-  `ends` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `usernameix` (`username`),
-  KEY `nameix` (`groupname`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
