9db42a91403fb0f79ce633caa45908b8b6626762
Rob Tierolff <teh@borreh.nl>
2016-04-28 22:48:07 +0200

    Restored original seed, cropped main article

--- database/base.sql
+++ database/base.sql
@@ -30,7 +30,8 @@ CREATE TABLE `Article` (
   KEY `urlindex` (`url`(255)),
   KEY `artloc_idx` (`locale`),
   CONSTRAINT `artloc` FOREIGN KEY (`locale`) REFERENCES `Locale` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Behaald`
@@ -46,12 +47,14 @@ CREATE TABLE `Behaald` (
   `waarde` int(11) DEFAULT NULL,
   `moment` datetime DEFAULT NULL,
   `obsolete` datetime DEFAULT NULL,
+  `author` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_Behaald_1` (`team`),
   KEY `fk_Behaald_2` (`prestatie`),
   CONSTRAINT `fk_Behaald_1` FOREIGN KEY (`team`) REFERENCES `Team` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_Behaald_2` FOREIGN KEY (`prestatie`) REFERENCES `Prestatie` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+) ENGINE=InnoDB AUTO_INCREMENT=423 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Body`
@@ -69,12 +72,14 @@ CREATE TABLE `Body` (
   `previous` int(11) DEFAULT NULL,
   `since` datetime DEFAULT NULL,
   `until` datetime DEFAULT NULL,
+  `authorusername` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `sectionarticle_idx` (`article`),
   KEY `previousversion_idx` (`previous`),
   CONSTRAINT `previousversion` FOREIGN KEY (`previous`) REFERENCES `Body` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `sectionarticle` FOREIGN KEY (`article`) REFERENCES `Article` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Temporary view structure for view `CurrentTeams`
@@ -102,6 +107,27 @@ SET character_set_client = utf8;
 SET character_set_client = @saved_cs_client;
 
 --
+-- Table structure for table `GroupGrant`
+--
+
+DROP TABLE IF EXISTS `GroupGrant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `GroupGrant` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(45) DEFAULT NULL,
+  `resource` varchar(45) DEFAULT NULL,
+  `subresource` varchar(45) DEFAULT NULL,
+  `action` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `resix` (`resource`),
+  KEY `subresix` (`subresource`),
+  KEY `actionix` (`action`),
+  KEY `gnix` (`groupname`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `Locale`
 --
 
@@ -114,7 +140,26 @@ CREATE TABLE `Locale` (
   `country` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `langct` (`language`,`country`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Login`
+--
+
+DROP TABLE IF EXISTS `Login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `passwordhash` varchar(512) DEFAULT NULL,
+  `person` int(11) DEFAULT NULL,
+  `password` varchar(250) DEFAULT NULL,
+  `salt` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Person`
@@ -127,7 +172,8 @@ CREATE TABLE `Person` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `valid` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `PersonAttribute`
@@ -147,7 +193,8 @@ CREATE TABLE `PersonAttribute` (
   KEY `attributename` (`name`),
   FULLTEXT KEY `attributevalue` (`value`),
   CONSTRAINT `personattributeperson` FOREIGN KEY (`person`) REFERENCES `Person` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+) ENGINE=InnoDB AUTO_INCREMENT=356 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `PersonView`
@@ -165,7 +212,29 @@ CREATE TABLE `PersonView` (
   PRIMARY KEY (`id`),
   KEY `nameindex` (`view`),
   KEY `viewattribute_idx` (`name`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Persoon`
+--
+
+DROP TABLE IF EXISTS `Persoon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Persoon` (
+  `int` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(256) NOT NULL,
+  `voornaam` varchar(256) NOT NULL,
+  `tussenvoegsel` varchar(45) DEFAULT NULL,
+  `achternaam` varchar(256) NOT NULL,
+  `buddy` int(11) DEFAULT NULL,
+  `secret` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`int`),
+  KEY `persoonbuddy_idx` (`buddy`),
+  CONSTRAINT `persoonbuddy` FOREIGN KEY (`buddy`) REFERENCES `Persoon` (`int`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Prestatie`
@@ -182,8 +251,10 @@ CREATE TABLE `Prestatie` (
   `docs` text,
   `starts` datetime DEFAULT NULL,
   `ends` datetime DEFAULT NULL,
+  `multiplier` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Session`
@@ -201,7 +272,8 @@ CREATE TABLE `Session` (
   PRIMARY KEY (`id`),
   KEY `loggedinsession_idx` (`login`),
   CONSTRAINT `loggedinsession` FOREIGN KEY (`login`) REFERENCES `Login` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+) ENGINE=InnoDB AUTO_INCREMENT=690 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `Team`
@@ -218,7 +290,8 @@ CREATE TABLE `Team` (
   `entered` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `tokenindex` (`token`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `TokenSection`
@@ -230,4 +303,71 @@ DROP TABLE IF EXISTS `TokenSection`;
 CREATE TABLE `TokenSection` (
   `section` varchar(45) NOT NULL,
   PRIMARY KEY (`section`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `UserGroup`
+--
+
+DROP TABLE IF EXISTS `UserGroup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UserGroup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `starts` datetime DEFAULT NULL,
+  `ends` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usernameix` (`username`),
+  KEY `nameix` (`groupname`)
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Final view structure for view `CurrentTeams`
+--
+
+/*!50001 DROP VIEW IF EXISTS `CurrentTeams`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`stortneer`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `CurrentTeams` AS (select `Team`.`id` AS `id`,`Team`.`title` AS `title` from (`CurrentTokens` join `Team` on((`CurrentTokens`.`token` = `Team`.`token`))) where (isnull(`Team`.`obsolete`) or (`Team`.`obsolete` > now()))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `CurrentTokens`
+--
+
+/*!50001 DROP VIEW IF EXISTS `CurrentTokens`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`stortneer`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `CurrentTokens` AS (select distinct `PersonAttribute`.`value` AS `token` from (`PersonAttribute` join `Person` on((`PersonAttribute`.`person` = `Person`.`id`))) where ((`PersonAttribute`.`obsolete` = 0) and (`PersonAttribute`.`name` = 'token') and (`Person`.`valid` = 1))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-28 22:45:59
