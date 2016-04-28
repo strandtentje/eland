57e77f37c9e04618b83c43b04cbeae27f60937b6
Rob Tierolff <teh@borreh.nl>
2016-04-28 22:51:34 +0200

    Removed views again

--- database/base.sql
+++ database/base.sql
@@ -81,35 +81,6 @@ CREATE TABLE `Body` (
 ) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Temporary view structure for view `CurrentTeams`
---
-
-DROP TABLE IF EXISTS `CurrentTeams`;
-/*!50001 DROP VIEW IF EXISTS `CurrentTeams`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `CurrentTeams` AS SELECT 
- 1 AS `id`,
- 1 AS `title`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Temporary view structure for view `CurrentTokens`
---
-
-DROP TABLE IF EXISTS `CurrentTokens`;
-/*!50001 DROP VIEW IF EXISTS `CurrentTokens`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `CurrentTokens` AS SELECT 
- 1 AS `token`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Table structure for table `GroupGrant`
---
-
 DROP TABLE IF EXISTS `GroupGrant`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -325,49 +296,3 @@ CREATE TABLE `UserGroup` (
 ) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Final view structure for view `CurrentTeams`
---
-
-/*!50001 DROP VIEW IF EXISTS `CurrentTeams`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`stortneer`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `CurrentTeams` AS (select `Team`.`id` AS `id`,`Team`.`title` AS `title` from (`CurrentTokens` join `Team` on((`CurrentTokens`.`token` = `Team`.`token`))) where (isnull(`Team`.`obsolete`) or (`Team`.`obsolete` > now()))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `CurrentTokens`
---
-
-/*!50001 DROP VIEW IF EXISTS `CurrentTokens`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`stortneer`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `CurrentTokens` AS (select distinct `PersonAttribute`.`value` AS `token` from (`PersonAttribute` join `Person` on((`PersonAttribute`.`person` = `Person`.`id`))) where ((`PersonAttribute`.`obsolete` = 0) and (`PersonAttribute`.`name` = 'token') and (`Person`.`valid` = 1))) */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-04-28 22:45:59
